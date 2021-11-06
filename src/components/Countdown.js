c972f744295c8d2f2dec5af839d08c9eae5b3f7d
Ankit Tyagi <you@example.com>
2021-11-06 17:04:54 +0530

    initial commit

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -52,7 +52,7 @@ function Countdown() {
       <div className="container">
         <div className="row">
           <div className="section-head col-md-12">
-            <h4>We will become a family in</h4>
+            <h4 style={{ fontWeight: "bold" }}>We will become a family in</h4>
           </div>
         </div>
         <div className="row">
